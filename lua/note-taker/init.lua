3e2c1734e5fea2caaeadf5d8e40f073809e5f5bc
Sckab <giulianodeamicis2009@gmail.com>
2026-07-13 17:45:27 +0200

    fix: directory creation

--- lua/note-taker/init.lua
+++ lua/note-taker/init.lua
@@ -22,7 +22,7 @@ M.setup = function(opts)
     M.opts = vim.tbl_deep_extend("force", default_opts, opts or {})
     M.opts.path = vim.fn.expand(M.opts.path)
 
-    vim.fn.mkdir(M.opts.path)
+    vim.uv.fs_mkdir(M.opts.path, 0755)
 
     local json_path = M.opts.path .. "notes.json"
 
