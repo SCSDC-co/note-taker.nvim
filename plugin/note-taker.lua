18674a41ccc619129e176b615ab447ead6ae96be
Sckab <giulianodeamicis2009@gmail.com>
2026-07-15 07:49:27 +0200

    revert to the last commit + remove useless condition

--- plugin/note-taker.lua
+++ plugin/note-taker.lua
@@ -1,13 +1,13 @@
 local note_taker = require("note-taker")
 
 vim.api.nvim_create_user_command("Note", function(opts)
-    if opts.count == 0 or opts.args == "list" then
+    if opts.args == "list" then
         note_taker.show_notes()
     elseif opts.args == "new" then
         note_taker.create_note()
     end
 end, {
-    nargs = 0,
+    nargs = 1,
 
     complete = function(ArgLead, CmdLine, CursorPos)
         return { "new", "list" }
