e8d5a41a556073ac88aaa82810441401634cf77e
Sckab <giulianodeamicis2009@gmail.com>
2026-07-15 08:16:59 +0200

    feat: add remove to the cmdline interface

--- plugin/note-taker.lua
+++ plugin/note-taker.lua
@@ -5,11 +5,13 @@ vim.api.nvim_create_user_command("Note", function(opts)
         note_taker.show_notes()
     elseif opts.args == "new" then
         note_taker.create_note()
+    elseif opts.args == "remove" then
+        note_taker.remove_notes()
     end
 end, {
     nargs = 1,
 
     complete = function(ArgLead, CmdLine, CursorPos)
-        return { "new", "list" }
+        return { "new", "list", "remove" }
     end,
 })
